7f2eec05e03a105bdef20563f7289423aa0620e2
Sigit Ari Prasetyo <sigitari111@gmial.com>
2019-08-25 15:52:13 +0700

    menambahkan cariPelaku.js dan haspusSimbol.js

---
+++ cariPelaku.js
@@ -0,0 +1,13 @@
+function cariPelaku(str) {
+    // you can only write your code here!
+    var regex = /abc/g
+    var hasil = str.match(regex, '')
+    return hasil.length
+  }
+  
+  // TEST CASES
+  console.log(cariPelaku('mabcvabc')); // 2
+  console.log(cariPelaku('abcdabdc')); // 1
+  console.log(cariPelaku('bcabcac')); // 1
+  console.log(cariPelaku('abcabcabc')); // 3
+  console.log(cariPelaku('babcbacabc')); // 2
